6c204f08fcdf50db0aa0921f231624767acda3d1
thebrianemory <the@brianemory.com>
2017-03-07 11:49:10 -0800

    Create user_homepage spec

---
+++ spec/features/user_views_homepage_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature 'User views homepage:' do
+  scenario 'they see "Hello World"' do
+    visit root_path
+
+    expect(page).to have_content t('hello')
+  end
+end
