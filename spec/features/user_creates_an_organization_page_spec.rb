47ead69975380cca9c104883bc9c74f5f83bd5e5
thebrianemory <the@brianemory.com>
2017-03-09 19:02:34 -0800

    Change spec to creating organization

---
+++ spec/features/user_creates_an_organization_page_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'User creates an organization' do
+  scenario 'they see the page for the created organization' do
+    org_name = 'Nekter Juice Bar'
+    org_email = 'njb@example.com'
+    org_creator = 'Jane Doe'
+    org_password = 'password'
+    org_password_confirmation = 'password'
+
+    visit root_path
+    click_on 'Create an organization'
+    fill_in 'organization_name', with: org_name
+    fill_in 'organization_email', with: org_email
+    fill_in 'organization_creator', with: org_creator
+    fill_in 'organization_password', with: org_password
+    fill_in 'organization_password_confirmation', with: org_password_confirmation
+    click_on 'Submit'
+
+    expect(page).to have_content(org_name)
+  end
+end
--- spec/features/user_views_create_organization_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'User views create organization page' do
-  scenario 'they see form for creating organzation' do
-    visit new_organization_path
-
-    expect(page).to have_tag('form')
-  end
-end
