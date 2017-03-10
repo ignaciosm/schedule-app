8d7e4806ec5e0121d878641f25416e81e925abfa
thebrianemory <the@brianemory.com>
2017-03-09 19:28:15 -0800

    Add create action

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,4 +2,21 @@ class OrganizationsController < ApplicationController
   def new
     @organization = Organization.new
   end
+
+  def create
+    @organization = Organization.new(organization_params)
+
+    if @organization.save
+      flash[:success] = 'You have successfully created your organization'
+      redirect_to organization_path(@organization)
+    else
+      render :new
+    end
+  end
+
+  private
+  def organization_params
+    params.require(:organization).permit(:name, :email, :organizer,
+                                         :password, :password_digest)
+  end
 end
