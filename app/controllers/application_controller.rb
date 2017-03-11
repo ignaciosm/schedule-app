aa58c4c1f3470197dc408c9983bed441ba53ecd2
thebrianemory <the@brianemory.com>
2017-03-11 13:02:02 -0800

    Add after_sign_out method for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     admin_path(current_admin)
   end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
