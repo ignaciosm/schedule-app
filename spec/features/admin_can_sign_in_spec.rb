49e24e4746f804fdfbe2116e528213becbbfd871
thebrianemory <the@brianemory.com>
2017-03-11 13:44:13 -0800

    Update test to use Devise helpers

--- spec/features/admin_can_sign_in_spec.rb
+++ spec/features/admin_can_sign_in_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature 'Admin can sign in' do
-  scenario 'they see their admin page' do
+  scenario 'then they see their admin page' do
     admin = create(:admin)
-    admin_email = admin.email
-    admin_password = admin.password
-
-    visit root_path
-    click_on 'Sign in'
-    fill_in 'admin_email', with: admin_email
-    fill_in 'admin_password', with: admin_password
-    click_on 'Log In'
+    sign_in admin
 
     expect(page).to have_content t('admin.show.greeting')
   end
