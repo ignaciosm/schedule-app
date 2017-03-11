2ee3f407eea5300b75559d68b6a1a8c024594b7d
thebrianemory <the@brianemory.com>
2017-03-11 13:24:50 -0800

    Add devise helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+
   config.include I18nMacros
 
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
