a3c3f55bc9dee9895556c4385a05bb3fb8c77369
thebrianemory <the@brianemory.com>
2017-03-11 15:49:44 -0800

    Update test to view all schedules

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'User views homepage:' do
-  scenario 'they see "Hello World"' do
+  scenario 'they see a list of schedules' do
+    schedule = create(:schedule)
+
     visit root_path
 
-    expect(page).to have_content t('welcome.home.greeting')
+    expect(page).to have_link schedule.year, href: schedule_path(schedule)
   end
 end
