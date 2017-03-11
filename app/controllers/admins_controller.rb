26b6a5f9259ede0fa16826a5ed5ab85a22d7d41d
thebrianemory <the@brianemory.com>
2017-03-11 13:08:36 -0800

    Add show action

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,3 +1,11 @@
 class AdminsController < ApplicationController
+  before_action :authenticate_admin!, :set_admin
 
+  def show
+  end
+
+  private
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
 end
