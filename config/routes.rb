a50f5835cfb6364bc7d7abd62682e22506a08d1f
thebrianemory <the@brianemory.com>
2017-03-11 13:19:25 -0800

    Remove registrations controller for devise route

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
                         sign_in: 'signin',
                         sign_out: 'signout',
                         sign_up: 'signup'
-                      },
-                      controllers: {
-                        registrations: 'registrations'
                       }
   resources :admins, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
