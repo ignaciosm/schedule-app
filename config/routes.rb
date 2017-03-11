b2f3e6e78096300bbb1910ae4bd1a2ef02e33cfe
thebrianemory <the@brianemory.com>
2017-03-11 13:18:28 -0800

    Update routes for devise actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
 
-  devise_for :admins
+  devise_for :admins, path: '',
+                      path_names: {
+                        sign_in: 'signin',
+                        sign_out: 'signout',
+                        sign_up: 'signup'
+                      },
+                      controllers: {
+                        registrations: 'registrations'
+                      }
   resources :admins, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
