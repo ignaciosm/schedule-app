382f9ef7aa13c481d9e35c762f2801a7bfd3cf37
thebrianemory <the@brianemory.com>
2017-03-11 12:05:36 -0800

    Create test for admin log in

---
+++ spec/features/admin_can_sign_in_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Admin can sign in' do
+  scenario 'they see their admin page' do
+    admin_email = 'demo@example.com'
+    admin_password = 'demoPass1'
+
+    visit new_user_session_path
+    fill_in 'admin_email', with: admin_email
+    fill_in 'admin_password', with: admin_password
+    click_on 'Log In'
+
+    expect(page).to have_content t('admin.show.greeting')
+  end
+end
