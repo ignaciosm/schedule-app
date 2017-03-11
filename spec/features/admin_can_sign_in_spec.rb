acfb49b21578aa5e18acfb09279526d646871972
thebrianemory <the@brianemory.com>
2017-03-11 13:02:45 -0800

    Update test to use nav sign in link

--- spec/features/admin_can_sign_in_spec.rb
+++ spec/features/admin_can_sign_in_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.feature 'Admin can sign in' do
   scenario 'they see their admin page' do
     admin = create(:admin)
+    admin_email = admin.email
+    admin_password = admin.password
 
-    visit new_admin_session_path
-    fill_in 'admin_email', with: admin.email
-    fill_in 'admin_password', with: admin.password
-    click_on 'Log in'
+    visit root_path
+    click_on 'Sign in'
+    fill_in 'admin_email', with: admin_email
+    fill_in 'admin_password', with: admin_password
+    click_on 'Log In'
 
     expect(page).to have_content t('admin.show.greeting')
   end
