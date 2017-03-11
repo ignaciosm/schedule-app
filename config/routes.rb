f83cb4ee7e6cde39204e4ddebee97896b9698c78
thebrianemory <the@brianemory.com>
2017-03-11 15:39:44 -0800

    Hide ability to get to the devise sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
                       path_names: {
                         sign_in: 'signin',
                         sign_out: 'signout',
-                        sign_up: 'signup'
+                        sign_up: 'signup_for_an_account_admin'
                       }
   resources :admins, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
