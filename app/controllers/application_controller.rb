5342393d1280099705e8a60bdcec88b9d87c2187
thebrianemory <the@brianemory.com>
2017-03-11 12:26:28 -0800

    Change redirect for admin sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  def after_sign_in_path_for(resource)
+    admin_path(current_admin)
+  end
 end
