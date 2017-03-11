c56d70604eeec5774ea2a7373fe1aa50a3ddcb6c
thebrianemory <the@brianemory.com>
2017-03-11 13:01:31 -0800

    Add admin show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
   devise_for :admins
+  resources :admins, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#home'
