b94c44e91b75401f01d5cd18f7a192b84caeb822
thebrianemory <the@brianemory.com>
2017-03-11 12:26:05 -0800

    Update to use factory

--- spec/features/admin_can_sign_in_spec.rb
+++ spec/features/admin_can_sign_in_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.feature 'Admin can sign in' do
   scenario 'they see their admin page' do
-    admin_email = 'demo@example.com'
-    admin_password = 'demoPass1'
+    admin = create(:admin)
 
     visit new_admin_session_path
-    fill_in 'admin_email', with: admin_email
-    fill_in 'admin_password', with: admin_password
-    click_on 'Log In'
+    fill_in 'admin_email', with: admin.email
+    fill_in 'admin_password', with: admin.password
+    click_on 'Log in'
 
     expect(page).to have_content t('admin.show.greeting')
   end
