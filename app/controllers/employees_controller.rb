d33886a30d2e21186df282e897ddeb5789e2d806
thebrianemory <the@brianemory.com>
2017-03-12 10:24:27 -0700

    Create show action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,5 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show]
 
   def new
     @employee = Employee.new
@@ -14,8 +15,15 @@ class EmployeesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :position)
+  end
+
+  def set_employee
+    @employee = Employee.find(params[:id])
   end
 end
