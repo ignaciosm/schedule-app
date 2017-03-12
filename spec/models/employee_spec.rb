2faafdfa479f9a074bfdca8e7bc57836454b2ce7
thebrianemory <the@brianemory.com>
2017-03-12 16:49:17 -0700

    Create test for .shift_leads

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,3 +3,12 @@ RSpec.describe Employee, 'validations' do
   it {is_expected.to validate_presence_of(:last_name) }
   it {is_expected.to validate_presence_of(:position) }
 end
+
+RSpec.describe Employee, '.shift_leads' do
+  it 'returns only the employees who are a shift lead' do
+    shift_lead = create(:employee)
+    team_member = create(:employee, :team_member)
+
+    expect(Employee.shift_leads).to eq [shift_lead]
+  end
+end
