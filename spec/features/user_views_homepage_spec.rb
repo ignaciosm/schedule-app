eb57f3ca4f15dbf425f2be078ce34f00b2cd4a75
thebrianemory <the@brianemory.com>
2017-03-11 16:39:42 -0800

    Refactor link text

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.feature 'User views homepage:' do
   scenario 'they see a list of schedules' do
     schedule = create(:schedule)
+    link_text = "Week #{schedule.biz_week}, #{schedule.biz_year}"
 
     visit root_path
 
-    expect(page).to have_link schedule.biz_year, href: schedule_path(schedule)
+    expect(page).to have_link link_text, href: schedule_path(schedule)
   end
 end
