f40f61b1dc74e1ed6047882bfc4d46cb1362b7ee
thebrianemory <the@brianemory.com>
2017-03-12 10:21:38 -0700

    Add create action with strong params

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,4 +3,13 @@ class EmployeesController < ApplicationController
   def new
     @employee = Employee.new
   end
+
+  def create
+    @employee = Employee.new(employee_params)
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :position)
+  end
 end
