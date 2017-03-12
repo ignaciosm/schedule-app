bfcafe16e2e8b5871495455242401b5724820848
thebrianemory <the@brianemory.com>
2017-03-12 08:45:35 -0700

    Rewrite schedule view test, update code to pass

--- spec/features/user_clicks_schedule_to_view_spec.rb
+++ spec/features/user_clicks_schedule_to_view_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'User clicks schedule link' do
     visit root_path
     click_on 'Week 10, 2017'
 
-    expect(page).to have_content t('schedules.show.header',
+    expect(page).to have_content t('schedules.table.header_days.header',
                                    week: schedule.biz_week)
   end
 end
