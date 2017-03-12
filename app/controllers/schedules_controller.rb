7e0a91d8e69543dfa2bcee6e802c612395e377db
thebrianemory <the@brianemory.com>
2017-03-11 16:58:45 -0800

    Add schedules show action

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,15 @@
 class SchedulesController < ApplicationController
+  before_action :set_schedule, only: [:show]
 
   def index
     @schedules = Schedule.all
+  end
+
+  def show
+  end
+
+  private
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
   end
 end
