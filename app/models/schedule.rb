8b49d4805bb2cf4a4f2223f4aa2df65c5bba7697
thebrianemory <the@brianemory.com>
2017-03-12 11:43:49 -0700

    Extract grabbing of dates to get_dates method

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,4 +4,8 @@ class Schedule < ApplicationRecord
   def week_and_year
     "Week #{biz_week}, #{biz_year}"
   end
+
+  def get_dates(year, week, day)
+    Date.commercial(year, week, day).strftime('%m/%d')
+  end
 end
