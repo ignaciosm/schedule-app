95ceeb436981e434305ce8475bdfa5c882614cc5
thebrianemory <the@brianemory.com>
2017-03-12 09:06:04 -0700

    Add test to look for table header info

--- spec/features/user_clicks_schedule_to_view_spec.rb
+++ spec/features/user_clicks_schedule_to_view_spec.rb
@@ -2,12 +2,31 @@ require 'rails_helper'
 
 RSpec.feature 'User clicks schedule link' do
   scenario 'they see the schdule' do
-    schedule = create(:schedule)
+    @schedule = create(:schedule)
+
+    visit root_path
+    click_on 'Week 10, 2017'
+
+    expect(page).to have_content t('schedules.show.greeting',
+                                   date1: get_date1, date7: get_date7,
+                                   year: @schedule.biz_year)
+  end
+
+  scenario 'they see the week in the table header' do
+    @schedule = create(:schedule)
 
     visit root_path
     click_on 'Week 10, 2017'
 
     expect(page).to have_content t('schedules.table.header_days.header',
-                                   week: schedule.biz_week)
+                                   week: @schedule.biz_week)
   end
+end
+
+def get_date1
+  Date.commercial(@schedule.biz_year, @schedule.biz_week, 1).strftime('%m/%d')
+end
+
+def get_date7
+  Date.commercial(@schedule.biz_year, @schedule.biz_week, 7).strftime('%m/%d')
 end
