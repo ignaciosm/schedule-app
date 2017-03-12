d7df52e241e0b91430046b7bfbc6b49765f4a0cd
thebrianemory <the@brianemory.com>
2017-03-11 17:00:11 -0800

    Fix typo in spec

--- spec/features/user_clicks_schedule_to_view_spec.rb
+++ spec/features/user_clicks_schedule_to_view_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature 'User clicks schedule link' do
     visit root_path
     click_on 'Week 10, 2017'
 
-    expect(page).to have_content t('schedules.show.header', week: schedule.week)
+    expect(page).to have_content t('schedules.show.header',
+                                   week: schedule.biz_week)
   end
 end
