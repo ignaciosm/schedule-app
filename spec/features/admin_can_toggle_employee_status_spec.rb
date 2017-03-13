a539bec7d83e564a65f54c3b31beb9cba528bf9b
thebrianemory <the@brianemory.com>
2017-03-13 11:20:40 -0700

    Add test to activate employee

--- spec/features/admin_can_toggle_employee_status_spec.rb
+++ spec/features/admin_can_toggle_employee_status_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'admin can toggle employee status' do
-  scenario 'clicking deactivate changes status to deactive' do
+  scenario 'clicking deactivate changes status to inactive' do
     admin = create(:admin)
     sign_in admin
     employee = create(:employee, admin_id: admin.id)
@@ -10,5 +10,16 @@ RSpec.feature 'admin can toggle employee status' do
     click_on 'deactivate'
 
     expect(page).to have_css '.inactive_employee'
+  end
+
+  scenario 'clicking activate changes status to active' do
+    admin = create(:admin)
+    sign_in admin
+    employee = create(:employee, admin_id: admin.id)
+
+    visit admin_path(admin)
+    click_on 'activate'
+
+    expect(page).to have_css '.active_employee'
   end
 end
