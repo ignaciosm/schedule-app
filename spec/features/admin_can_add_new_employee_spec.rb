2f888b2190e2f3329145767573ebbaa40965f897
thebrianemory <the@brianemory.com>
2017-03-12 17:48:17 -0700

    Update test to use .first_name_last_initial

--- spec/features/admin_can_add_new_employee_spec.rb
+++ spec/features/admin_can_add_new_employee_spec.rb
@@ -4,22 +4,19 @@ RSpec.feature 'Admin can create new employee' do
   scenario 'they can see the employee\'s page' do
     admin = create(:admin)
     sign_in admin
-    @employee_first_name = 'Tony'
-    @employee_last_name = 'Stark'
-    @employee_position = 'Shift Lead'
+    employee_first_name = 'Tony'
+    employee_last_name = 'Stark'
+    employee_position = 'Shift Lead'
 
     visit root_path
     click_on t('layouts.navigation.admin_page')
     click_on t('admins.show.add_employee')
-    fill_in 'employee_first_name', with: @employee_first_name
-    fill_in 'employee_last_name', with: @employee_last_name
-    fill_in 'employee_position', with: @employee_position
+    fill_in 'employee_first_name', with: employee_first_name
+    fill_in 'employee_last_name', with: employee_last_name
+    fill_in 'employee_position', with: employee_position
     click_on t('employees.form.submit_button')
+    employee = Employee.last
 
-    expect(page).to have_content get_short_name
+    expect(page).to have_content employee.first_name_last_initial
   end
-end
-
-def get_short_name
-  "#{@employee_first_name} #{@employee_last_name[0]}."
 end
