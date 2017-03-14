986fd5a7712fea0a00da1a22a49e811c02215ee7
thebrianemory <the@brianemory.com>
2017-03-13 20:14:39 -0700

    Create test for editting employee

---
+++ spec/features/admin_can_edit_employee_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'Admin can edit employee' do
+  scenario 'they can see the employee\'s page' do
+    admin = create(:admin)
+    sign_in admin
+    employee = create(:employee, admin_id: admin.id)
+
+    visit root_path
+    click_on t('layouts.navigation.admin_page')
+    click_on t('admins.show.edit_employee')
+    fill_in 'employee_position', with: 'Team Member'
+    click_on t('employees.form.submit_button')
+    employee = Employee.last
+
+    expect(employee.status).to eq 'Team Member'
+    expect(page).to have_content employee.first_name_last_initial
+  end
+end
