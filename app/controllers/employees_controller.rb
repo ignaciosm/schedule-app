26808fcb8502921832edc6dd3c39cbf58602a16c
thebrianemory <the@brianemory.com>
2017-03-13 19:56:59 -0700

    Fix typo

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-  before_action :set_employee, only: [:show, :toggle_statuss]
+  before_action :set_employee, only: [:show, :toggle_status]
 
   def new
     @employee = Employee.new
