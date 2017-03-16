8b97ea8fad6f96ba4415986fdecfd5495f0fd4f6
thebrianemory <the@brianemory.com>
2017-03-15 22:48:26 -0700

    Add test to unassign shifts

--- spec/features/admin_can_toggle_active_shifts_belong_to_a_schedule_spec.rb
+++ spec/features/admin_can_toggle_active_shifts_belong_to_a_schedule_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'admin can toggle active shifts belong to a schedule' do
-  scenario 'by clicking button on schedule page' do
+  scenario 'by clicking button on schedule page to assign' do
     admin = create(:admin)
     sign_in admin
     schedule = create(:schedule, admin_id: admin.id)
@@ -12,5 +12,18 @@ RSpec.feature 'admin can toggle active shifts belong to a schedule' do
     click_on t('schedules.show.assign_shifts')
 
     expect(page).to have_content shift.start_time
+  end
+
+  scenario 'by clicking button on schedule page to unassign' do
+    admin = create(:admin)
+    sign_in admin
+    schedule = create(:schedule, admin_id: admin.id)
+    shift = create(:shift, admin_id: admin.id)
+
+    visit root_path
+    click_on schedule.week_and_year
+    click_on t('schedules.show.assign_shifts')
+
+    expect(page).to_not have_content shift.start_time
   end
 end
