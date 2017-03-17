1fe21c9e78de690e1ec3fb050a84b72ac0e75c25
thebrianemory <the@brianemory.com>
2017-03-17 11:47:35 -0700

    Create not_your_profile method for checking show action

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,11 +2,15 @@ class AdminsController < ApplicationController
   before_action :authenticate_admin!, :set_admin
 
   def show
-    redirect_to admin_path(current_admin) unless @admin.id == current_admin.id
+    redirect_to admin_path(current_admin) if not_your_profile
   end
 
   private
   def set_admin
     @admin = Admin.find(params[:id])
+  end
+
+  def not_your_profile
+    @admin.id != current_admin.id
   end
 end
