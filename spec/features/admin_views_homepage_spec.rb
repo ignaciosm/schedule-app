97edf83eb539bdecb1d8b6c0ac2b9e3dac3077ad
thebrianemory <the@brianemory.com>
2017-03-17 10:50:40 -0700

    Update test to only find admin's schedules

---
+++ spec/features/admin_views_homepage_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature 'Admin views homepage:' do
+  scenario 'they see a list of their schedules' do
+    admin = create(:admin)
+    schedule = create(:schedule, admin: admin)
+    schedule2 = create(:schedule, biz_week: 11)
+    link_text = "Week #{schedule.biz_week}, #{schedule.biz_year}"
+    link_text2 = "Week #{schedule2.biz_week}, #{schedule2.biz_year}"
+
+    visit root_path
+
+    expect(page).to_not have_link link_text2, href: schedule_path(schedule2)
+    expect(page).to have_link link_text, href: schedule_path(schedule)
+  end
+end
