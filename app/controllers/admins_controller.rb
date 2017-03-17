78b1fe0af6299499087295651cd4fd08020b68e9
thebrianemory <the@brianemory.com>
2017-03-17 11:35:37 -0700

    Admin can only see their profile page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,7 @@ class AdminsController < ApplicationController
   before_action :authenticate_admin!, :set_admin
 
   def show
+    redirect_to root_path unless @admin.id == current_admin.id
   end
 
   private
