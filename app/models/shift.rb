0cad5c0508a656c1881a0725e0bbc74752d3c861
thebrianemory <the@brianemory.com>
2017-03-16 17:29:44 -0700

    Add method for active_shifts

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,9 @@ class Shift < ApplicationRecord
   belongs_to :admin
   has_many :schedule_shifts
   has_many :schedules, through: :schedule_shifts
+
+  def self.active_shifts
+    where('status = ?', 'active')
+  end
+
 end
