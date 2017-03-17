ab1d47942add89a65e401dafde61c0fd2b783265
thebrianemory <the@brianemory.com>
2017-03-17 14:13:19 -0700

    Create test for adding new schedule

---
+++ spec/features/admin/admin_can_create_a_schedule_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'admin can create a schedule' do
+  scenario 'they can see the schedule on the index page' do
+    admin = create(:admin)
+    sign_in admin
+    schedule_year = 2017
+    schedule_week = 13
+
+    visit admin_path(admin)
+    click_on t('admins.show.add_schedule')
+    fill_in 'schedule_biz_year', with: schedule_year
+    fill_in 'schedule_biz_week', with: schedule_week
+    click_on t('schedule.form.submit_button')
+
+    expect(page).to have_content schedule.week_and_year
+  end
+end
