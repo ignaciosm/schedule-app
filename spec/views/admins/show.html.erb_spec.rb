1a2d87ca6afa4033948a0344a0ca4f039b04de75
thebrianemory <the@brianemory.com>
2017-03-18 07:58:36 -0700

    Update spec to use build

--- spec/views/admins/show.html.erb_spec.rb
+++ spec/views/admins/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'admins/show.html.erb' do
   context 'shows the employees by position' do
     it 'has a shift lead section' do
-      @admin = create(:admin)
+      @admin = build(:admin)
 
       render
 
@@ -11,7 +11,7 @@ RSpec.describe 'admins/show.html.erb' do
     end
 
     it 'has a team member section' do
-      @admin = create(:admin)
+      @admin = build(:admin)
 
       render
 
