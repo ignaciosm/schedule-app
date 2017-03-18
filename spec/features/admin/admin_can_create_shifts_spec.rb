cf9760164ceadaaf91d63376f68c0261c4218ffc
thebrianemory <the@brianemory.com>
2017-03-18 14:37:41 -0700

    Update test for proper select usage

--- spec/features/admin/admin_can_create_shifts_spec.rb
+++ spec/features/admin/admin_can_create_shifts_spec.rb
@@ -4,14 +4,14 @@ RSpec.feature 'Admin can create shifts' do
   scenario 'they can see the shift on the shift index' do
     admin = create(:admin)
     sign_in admin
-    day_of_week = 'Monday'
+    day_of_week = 'shift_day_of_week_monday'
     start_time = '07:30'
     end_time = '12:00'
     position = 'Shift Lead'
 
     visit admin_path(admin)
     click_on t('admins.show.add_shift')
-    fill_in 'shift_day_of_week', with: day_of_week
+    check(day_of_week)
     fill_in 'shift_start_time', with: start_time
     fill_in 'shift_end_time', with: end_time
     select(position)
