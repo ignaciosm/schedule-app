cecb4075f8f62b3c69d6a5c05c249d04eec1c8d7
thebrianemory <the@brianemory.com>
2017-03-18 13:22:37 -0700

    Update spec to include password

--- spec/features/admin/admin_can_update_their_account_spec.rb
+++ spec/features/admin/admin_can_update_their_account_spec.rb
@@ -5,10 +5,12 @@ RSpec.feature 'admin can update their account' do
     admin = create(:admin)
     sign_in admin
     new_company = 'Batman Inc'
+    password = 'demoPass1@'
 
     visit admin_path(admin)
     click_on t('admins.show.update_account')
     fill_in 'admin_company', with: new_company
+    fill_in 'admin_current_password', with: password
     click_on 'Update'
 
     expect(page).to have_content new_company
