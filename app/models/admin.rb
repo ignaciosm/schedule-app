0c639aefde06307ea0a6112f2b37b49e69bc2d0f
thebrianemory <the@brianemory.com>
2017-03-18 13:04:51 -0700

    Remove devise confirmable

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,8 +2,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :first_name, :last_name, :company
   validates_uniqueness_of :company
   has_many :schedules
