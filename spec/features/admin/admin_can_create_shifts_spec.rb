e437ff53d5c67ed670a97620a8185e83f7bfe0d4
thebrianemory <the@brianemory.com>
2017-03-19 12:42:28 -0700

    Update test to select employee and not position

--- spec/features/admin/admin_can_create_shifts_spec.rb
+++ spec/features/admin/admin_can_create_shifts_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature 'Admin can create shifts' do
     start_minute = '00'
     end_hour = '03 PM'
     end_minute = '00'
-    position = 'Shift Lead'
+    employee = create(:employee, admin: admin_can_add_and_remove_active_employees_to_a_schedule_spec.rb)
 
     visit admin_path(admin)
     click_on t('admins.show.add_shift')
@@ -20,7 +20,7 @@ RSpec.feature 'Admin can create shifts' do
     select start_minute, from: 'shift_start_time_5i'
     select end_hour, from: 'shift_end_time_4i'
     select end_minute, from: 'shift_end_time_5i'
-    select(position)
+    select(employee)
     click_on t('shifts.form.submit_button')
 
     expect(page).to have_content '11:00'
