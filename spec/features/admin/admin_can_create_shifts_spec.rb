bc5471a1347a310fba29cb11badd67c4e6a376dc
thebrianemory <the@brianemory.com>
2017-03-19 07:51:42 -0700

    Update test to use dropdowns for shift times

--- spec/features/admin/admin_can_create_shifts_spec.rb
+++ spec/features/admin/admin_can_create_shifts_spec.rb
@@ -6,19 +6,23 @@ RSpec.feature 'Admin can create shifts' do
     sign_in admin
     monday = 'shift_day_of_week_monday'
     friday = 'shift_day_of_week_friday'
-    start_time = '07:30'
-    end_time = '12:00'
+    start_time1 = '07'
+    start_time2 = '30'
+    end_time1 = '12'
+    end_time2 = '00'
     position = 'Shift Lead'
 
     visit admin_path(admin)
     click_on t('admins.show.add_shift')
     check(monday)
     check(friday)
-    fill_in 'shift_start_time', with: start_time
-    fill_in 'shift_end_time', with: end_time
+    select start_time1, from: 'shift_start_time_4i'
+    select start_time2, from: 'shift_start_time_5i'
+    select end_time1, from: 'shift_end_time_4i'
+    select end_time2, from: 'shift_end_time_5i'
     select(position)
     click_on t('shifts.form.submit_button')
 
-    expect(page).to have_content start_time
+    expect(page).to have_content '07:30'
   end
 end
