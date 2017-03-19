db506d124ccc8ca85e2a75573567f11926fb70d0
thebrianemory <the@brianemory.com>
2017-03-19 07:51:58 -0700

    Refactor shift form and controller for using dropdowns for times

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -11,7 +11,11 @@ class ShiftsController < ApplicationController
   def create
     params[:shift][:day_of_week].each do |day|
       @shift = Shift.new(shift_params)
-      @shift.attributes = { day_of_week: day, admin_id: current_admin.id }
+      @shift.attributes = { day_of_week: day,
+                            admin_id: current_admin.id,
+                            start_time: short_start_time,
+                            end_time: short_end_time
+                          }
 
       return render :new unless @shift.save
     end
@@ -22,5 +26,13 @@ class ShiftsController < ApplicationController
   def shift_params
     params.require(:shift).permit(:start_time, :end_time, :position,
                                   :day_of_week)
+  end
+
+  def short_start_time
+    shift_params['start_time(4i)'] + ':' + shift_params['start_time(5i)']
+  end
+
+  def short_end_time
+    shift_params['end_time(4i)'] + ':' + shift_params['end_time(5i)']
   end
 end
