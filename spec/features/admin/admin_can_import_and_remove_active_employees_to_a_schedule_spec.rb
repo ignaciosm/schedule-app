8fd8fe7fd1d5f626bb8a8046d236a3b7486fa711
thebrianemory <the@brianemory.com>
2017-03-18 20:47:04 -0700

    Update test to use wording import/remove and not assign/unassign

--- spec/features/admin/admin_can_import_and_remove_active_employees_to_a_schedule_spec.rb
+++ spec/features/admin/admin_can_import_and_remove_active_employees_to_a_schedule_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature 'admin can toggle active employees belong to a schedule' do
-  scenario 'by clicking button on schedule page to assign' do
+RSpec.feature 'admin can import and remove active employees to a schedule' do
+  scenario 'by clicking button on schedule page to import' do
     admin = create(:admin)
     sign_in admin
     schedule = create(:schedule, admin: admin)
     shift_lead = create(:employee, admin: admin)
 
     visit schedule_path(schedule)
-    click_on t('schedules.toggle_employees.assign_employees')
+    click_on t('schedules.toggle_employees.import_employees')
 
     expect(page).to have_content shift_lead.first_name_last_initial
   end
 
-  scenario 'by clicking button on schedule page to unassign' do
+  scenario 'by clicking button on schedule page to remove' do
     admin = create(:admin)
     sign_in admin
     schedule = create(:schedule, admin: admin)
     shift_lead = create(:employee, admin: admin)
 
     visit schedule_path(schedule)
-    click_on t('schedules.toggle_employees.assign_employees')
+    click_on t('schedules.toggle_employees.import_employees')
 
     visit schedule_path(schedule)
-    click_on t('schedules.toggle_employees.unassign_employees')
+    click_on t('schedules.toggle_employees.remove_employees')
 
     expect(page).to_not have_content shift_lead.first_name_last_initial
   end
--- spec/features/admin/admin_can_import_and_remove_active_shifts_to_a_schedule_spec.rb
+++ spec/features/admin/admin_can_import_and_remove_active_shifts_to_a_schedule_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature 'admin can toggle active shifts belong to a schedule' do
-  scenario 'by clicking button on schedule page to assign' do
+RSpec.feature 'admin can import/remove active shifts to a schedule' do
+  scenario 'by clicking button on schedule page to import' do
     admin = create(:admin)
     sign_in admin
     schedule = create(:schedule, admin: admin)
     shift = create(:shift, admin: admin)
 
     visit schedule_path(schedule)
-    click_on t('schedules.toggle_shifts.assign_shifts')
+    click_on t('schedules.toggle_shifts.import_shifts')
 
     expect(page).to have_content shift.start_time
   end
 
-  scenario 'by clicking button on schedule page to unassign' do
+  scenario 'by clicking button on schedule page to remove' do
     admin = create(:admin)
     sign_in admin
     schedule = create(:schedule, admin: admin)
     shift = create(:shift, admin: admin)
 
     visit schedule_path(schedule)
-    click_on t('schedules.toggle_shifts.assign_shifts')
+    click_on t('schedules.toggle_shifts.import_shifts')
 
     visit schedule_path(schedule)
-    click_on t('schedules.toggle_shifts.unassign_shifts')
+    click_on t('schedules.toggle_shifts.remove_shifts')
 
     expect(page).to_not have_content shift.start_time
   end
