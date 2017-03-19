1a48fdcd2a688104d81ba5dae7dd79da539adee2
thebrianemory <the@brianemory.com>
2017-03-19 16:31:29 -0700

    Implement #start_time_end_time

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -13,4 +13,8 @@ class Shift < ApplicationRecord
   def self.shifts_by_day(day)
     where('day_of_week = ?', day)
   end
+
+  def start_time_end_time
+    start_time.strftime('%I:%M %p') + ' to ' + end_time.strftime('%I:%M %p')
+  end
 end
