83410e829b6cdd72533830b66fe2d8a6732cfa04
thebrianemory <the@brianemory.com>
2017-03-20 21:02:09 -0700

    Update spec to look for employee shifts

--- spec/features/schedule_page_shows_employee_shifts_spec.rb
+++ spec/features/schedule_page_shows_employee_shifts_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature 'User views a schedule' do
-  scenario 'they see a section for each employee position' do
-    admin = create(:admin)
+  scenario 'they see the shifts for each employee' do
+    admin = build(:admin)
     sign_in admin
     schedule = create(:schedule, admin: admin)
     shift_lead = create(:employee, admin: admin)
     team_member = create(:employee, position: 'Team Member', admin: admin)
+    shift_lead.shifts << shift1 = create(:shift)
+    team_member.shifts << shift2 = create(:shift)
 
     visit schedule_path(schedule)
     click_on t('schedules.add_remove_employees.add_employees')
 
     expect(page).to have_content admin.first_name
-    expect(page).to have_content shift_lead.first_name_last_initial
-    expect(page).to have_content team_member.first_name_last_initial
+    expect(page).to have_content shift1.start_time_end_time
+    expect(page).to have_content shift2.start_time_end_time
   end
 end
