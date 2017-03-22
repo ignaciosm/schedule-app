ebc65dfab80b0b0c1a65265f860f570e4c0f22dd
thebrianemory <the@brianemory.com>
2017-03-22 10:48:29 -0700

    Update layout of routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,17 @@ Rails.application.routes.draw do
   resources :shifts, only: [:new, :create, :edit, :update, :index]
   resources :employees, only: [:show, :create, :new, :edit, :update]
 
-  post 'employees/:id/deactivate', to: 'employees#toggle_status',
-                                   as: 'deactivate'
-  post 'employees/:id/activate', to: 'employees#toggle_status',
-                                 as: 'activate'
+  post 'employees/:id/deactivate',        to: 'employees#toggle_status',
+                                          as: 'deactivate'
+  post 'employees/:id/activate',          to: 'employees#toggle_status',
+                                          as: 'activate'
 
-  post 'schedules/:id/add_employees', to: 'schedules#add_employees',
-                                      as: 'add_employees'
-  post 'schedules/:id/remove_employees', to: 'schedules#remove_employees',
-                                        as: 'remove_employees'
-  get 'schedules/:id/set_schedule', to: 'schedules#set_schedule', as: 'set_schedule'
+  post 'schedules/:id/add_employees',     to: 'schedules#add_employees',
+                                          as: 'add_employees'
+  post 'schedules/:id/remove_employees',  to: 'schedules#remove_employees',
+                                          as: 'remove_employees'
+  get 'schedules/:id/set_schedule',       to: 'schedules#set_schedule',
+                                          as: 'set_schedule'
 
   devise_for :admins, path: '',
                       path_names: {
@@ -25,6 +26,7 @@ Rails.application.routes.draw do
                         registrations: 'registrations'
                       }
   resources :admins, only: [:show]
+  resources :set_times
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :schedules, only: [:show, :index, :new, :update, :edit, :create]
