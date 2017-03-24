abed4737d50db10bbfb0ce8f65ffd63f035a9dec
thebrianemory <the@brianemory.com>
2017-03-24 08:50:26 -0700

    Adjust spec to fill in shift times

--- spec/features/admin/admin_can_set_a_schedule_spec.rb
+++ spec/features/admin/admin_can_set_a_schedule_spec.rb
@@ -11,8 +11,10 @@ RSpec.feature 'admin can set a schedule' do
     click_on t('schedules.add_remove_employees.add_employees')
 
     click_on t('schedules.show.edit_schedule')
+    fill_in 'schedule_shifts_attributes_0_start_time', with: '07:30 AM'
+    fill_in 'schedule_shifts_attributes_0_end_time', with: '04:00 PM'
     click_on 'Finalize schedule'
 
-    expect(page).to have_content 'Final schedule'
+    expect(page).to have_content '07:30 AM'
   end
 end
