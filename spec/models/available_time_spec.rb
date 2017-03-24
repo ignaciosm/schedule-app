cef782412a6d278bb1f820742510988949f7f739
thebrianemory <the@brianemory.com>
2017-03-23 21:01:56 -0700

    Change shift to available_time

---
+++ spec/models/available_time_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe AvailableTime, 'validations' do
+  it { is_expected.to validate_presence_of(:day_of_week) }
+  it { is_expected.to validate_presence_of(:start_time) }
+  it { is_expected.to validate_presence_of(:end_time) }
+end
+
+RSpec.describe AvailableTime, '.available_times_by_day' do
+  it 'returns the day of the week passed in' do
+    monday = create(:available_time)
+    create(:available_time, day_of_week: 'Tuesday')
+
+    expect(AvailableTime.available_times_by_day('Monday')).to eq [monday]
+  end
+end
+
+RSpec.describe AvailableTime, '#start_time_end_time' do
+  it 'returns the available_time times formatted' do
+    available_time = create(:available_time)
+
+    expect(available_time.start_time_end_time).to eq '11:00 AM to 03:00 PM'
+  end
+end
