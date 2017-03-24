12d322094050390be9ca59fb50638929452bd2cb
thebrianemory <the@brianemory.com>
2017-03-23 22:07:50 -0700

    Fix typo

--- db/migrate/20170319195226_create_employee_available_times.rb
+++ db/migrate/20170319195226_create_employee_available_times.rb
@@ -2,7 +2,7 @@ class CreateEmployeeAvailableTimes < ActiveRecord::Migration[5.0]
   def change
     create_table :employee_available_times do |t|
       t.references :employee, foreign_key: true
-      t.references :available_times, foreign_key: true
+      t.references :available_time, foreign_key: true
 
       t.timestamps
     end
