a1e0b40c482ea8144525faaf37be1f2b9ae44353
thebrianemory <the@brianemory.com>
2017-03-26 22:18:55 -0700

    Update spec to expect display of shift

--- spec/features/admin/admin_can_set_a_schedule_spec.rb
+++ spec/features/admin/admin_can_set_a_schedule_spec.rb
@@ -11,8 +11,10 @@ RSpec.feature 'admin can set a schedule' do
     click_on t('schedules.add_remove_employees.add_employees')
 
     click_on t('schedules.show.set_schedule')
+    fill_in 'schedule_shifts_attributes_0_start_time', with: '7:30 am'
+    fill_in 'schedule_shifts_attributes_0_end_time', with: '4:00 pm'
     click_on 'Finalize schedule'
 
-    expect(page).to have_content 'Finished schedule'
+    expect(page).to have_content '7:30 am to 4:00 pm'
   end
 end
