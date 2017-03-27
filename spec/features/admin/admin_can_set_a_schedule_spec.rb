f799bfbe58373b0f1e4f139daa5a1c6cf59dab41
thebrianemory <the@brianemory.com>
2017-03-27 16:08:31 -0700

    Clean up spec

--- spec/features/admin/admin_can_set_a_schedule_spec.rb
+++ spec/features/admin/admin_can_set_a_schedule_spec.rb
@@ -5,18 +5,19 @@ RSpec.feature 'admin can set a schedule' do
     admin = create(:admin)
     sign_in admin
     schedule = create(:schedule, admin: admin)
-    create(:employee, admin: admin)
-    create(:employee, position: 'Team Member', admin: admin)
-    visit schedule_path(schedule)
-    click_on t('schedules.add_remove_employees.add_employees')
+    schedule.employees << create(:employee, admin: admin)
+    schedule.employees << create(:employee,
+                                 position: 'Team Member',
+                                 admin: admin)
 
+    visit schedule_path(schedule)
     click_on t('schedules.show.set_schedule')
-    fill_in 'schedule_shifts_attributes_0_start_time', with: '7:30 am'
-    fill_in 'schedule_shifts_attributes_0_end_time', with: '4:00 pm'
-    fill_in 'schedule_shifts_attributes_7_start_time', with: '3:30 pm'
-    fill_in 'schedule_shifts_attributes_7_end_time', with: '7:30 pm'
+    fill_in 'schedule_shifts_attributes_0_start_time',  with: '7:30 am'
+    fill_in 'schedule_shifts_attributes_0_end_time',    with: '4:00 pm'
+    fill_in 'schedule_shifts_attributes_7_start_time',  with: '3:30 pm'
+    fill_in 'schedule_shifts_attributes_7_end_time',    with: '7:30 pm'
     fill_in 'schedule_shifts_attributes_14_start_time', with: '12:00 pm'
-    fill_in 'schedule_shifts_attributes_14_end_time', with: '4:00 pm'
+    fill_in 'schedule_shifts_attributes_14_end_time',   with: '4:00 pm'
     click_on 'Finalize schedule'
 
     expect(page).to have_content '7:30 am to 4:00 pm'
