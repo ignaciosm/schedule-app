772fb2218a11ded8b3f9b268470dc4b8e3840469
thebrianemory <the@brianemory.com>
2017-03-28 20:42:23 -0700

    Add spec for re-rendering edit page

--- spec/features/admin/admin_can_edit_employee_spec.rb
+++ spec/features/admin/admin_can_edit_employee_spec.rb
@@ -16,4 +16,18 @@ RSpec.feature 'Admin can edit employee' do
     expect(employee.position).to eq 'Team Member'
     expect(page).to have_content employee.first_name_last_initial
   end
+
+  scenario 're-renders edit page when submitting improper data' do
+    admin = create(:admin)
+    sign_in admin
+    create(:employee, admin: admin)
+
+    visit root_path
+    click_on t('layouts.navigation.admin_page')
+    click_on t('admins.show.edit_employee')
+    fill_in 'employee_first_name', with: ''
+    click_on 'Update Employee'
+
+    expect(page).to have_content 'First name can\'t be blank'
+  end
 end
