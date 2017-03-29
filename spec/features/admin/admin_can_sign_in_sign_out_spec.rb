d3c6f102c3abebcbaabac6dd79338a798d375863
thebrianemory <the@brianemory.com>
2017-03-28 22:20:39 -0700

    Add spec for signing out

--- spec/features/admin/admin_can_sign_in_sign_out_spec.rb
+++ spec/features/admin/admin_can_sign_in_sign_out_spec.rb
@@ -10,3 +10,15 @@ RSpec.feature 'Admin can sign in' do
     expect(page).to have_content t('admins.show.greeting', company: admin.company)
   end
 end
+
+RSpec.feature 'Admin can sign out' do
+  scenario 'they are redirect to the root path' do
+    admin = create(:admin)
+    sign_in admin
+
+    visit admin_path(admin)
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
