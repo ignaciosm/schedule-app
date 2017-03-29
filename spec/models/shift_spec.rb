9f2276362330123ff4988018da07e82feb681790
thebrianemory <the@brianemory.com>
2017-03-28 20:10:44 -0700

    Update spec for shifts_by_employee

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe Shift, '.shifts_by_day(day)' do
     shift2 = create(:shift, day_of_week: 'Tuesday')
 
     expect(Shift.shifts_by_day('Monday')).to eq [shift1]
-    expect(Shift.shifts_by_day('Tuesday')).to eq [shift2]
+    expect(Shift.shifts_by_day('Monday')).to_not eq [shift2]
   end
 end
 
 RSpec.describe Shift, 'shifts_by_employee(employee)' do
   it 'returns shift by employee' do
+    employee = create(:employee)
+    shift1 = create(:shift, employee_id: employee.id)
+    shift2 = create(:shift)
 
+    expect(Shift.shifts_by_employee(employee)).to eq [shift1]
+    expect(Shift.shifts_by_employee(employee)).to_not eq [shift2]
   end
 end
