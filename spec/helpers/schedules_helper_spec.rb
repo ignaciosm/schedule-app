e188869e0f7ddda0e08859ffdfb0a6682d7cd369
thebrianemory <the@brianemory.com>
2017-03-28 22:50:58 -0700

    Add spec to test schedules helper

---
+++ spec/helpers/schedules_helper_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe SchedulesHelper, '#shifts_created?(day, employee, schedule)' do
+  it 'returns shifts by day and employee' do
+    schedule = create(:schedule)
+    schedule.employees << shift_lead  = create(:employee)
+    schedule.shifts << shift = create(:shift, employee_id: shift_lead.id,
+                                     start_time:  '2:30 pm',
+                                     end_time:    '7:30 pm')
+
+    shifts = helper.shifts_created?('Monday', shift_lead, schedule)
+    expect(shifts).to eq [shift]
+  end
+end
