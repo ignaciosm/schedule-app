3af08115c1499a4c29093335339db04f5f60d10f
thebrianemory <the@brianemory.com>
2017-03-29 19:47:55 -0700

    Tidy up code in helpers

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,15 +1,11 @@
 module SchedulesHelper
 
   def employee_type(employee)
-    if employee.class == Admin
-      0
-    else
-      employee.id
-    end
+    employee.class == Admin ? 0 : employee.id
   end
 
   def shifts_created?(day, employee, schedule)
-    if schedule.shifts.shifts_by_day_and_employee(day, employee).count == 0
+    if schedule.shifts.shifts_by_day_and_employee(day, employee).count.zero?
       @schedule.shifts.build
     else
       schedule.shifts.shifts_by_day_and_employee(day, employee)
