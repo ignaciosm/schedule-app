f6da6d67bb8ef611e4295eaa4e26f00da3562fdd
thebrianemory <the@brianemory.com>
2017-04-02 21:57:25 -0700

    Remove unneeded db call

--- spec/features/admin/admin_can_add_new_employee_spec.rb
+++ spec/features/admin/admin_can_add_new_employee_spec.rb
@@ -15,9 +15,8 @@ RSpec.feature 'Admin can create new employee' do
     fill_in   'employee_last_name',   with: employee_last_name
     select(employee_position)
     click_on 'Create Employee'
-    employee = Employee.last
 
-    expect(page).to have_content employee.first_name_last_initial
+    expect(page).to have_content('Tony S.')
   end
 
   scenario 're-renders page when submitting improper data' do
