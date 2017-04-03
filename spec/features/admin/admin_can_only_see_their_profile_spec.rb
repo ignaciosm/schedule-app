d724431e4a37a43b98e039b0738f87c49d446a22
thebrianemory <the@brianemory.com>
2017-04-03 07:29:31 -0700

    Remove unneeded db calls

--- spec/features/admin/admin_can_only_see_their_profile_spec.rb
+++ spec/features/admin/admin_can_only_see_their_profile_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature 'admin can see only their profile' do
   scenario 'they are redirected when viewing another profile' do
-    admin1  = create(:admin)
+    admin1  = create(:admin, company: 'Wayne Enterprises')
     admin2  = create(:admin, company: 'Joker and Sons')
     sign_in admin1
 
     visit admin_path(admin2)
 
-    expect(page).to     have_content admin1.company
-    expect(page).to_not have_content admin2.company
+    expect(page).to     have_content('Wayne Enterprises')
+    expect(page).to_not have_content('Joker and Sons')
   end
 end
