2b1e05dac38df43420e202cd1dda5bb4e7549b9d
thebrianemory <the@brianemory.com>
2017-04-02 21:52:07 -0700

    Make spec more explicit and remove DB calls Remove database calls by having the expectation look for the name, i.e. Boaty M., instead of shift_lead.first_name_last_initial. Also made it clear that the created shift lead had the name of Boaty McBoatface without needing to look at the factory.

--- spec/features/admin/admin_can_add_and_remove_active_employees_to_a_schedule_spec.rb
+++ spec/features/admin/admin_can_add_and_remove_active_employees_to_a_schedule_spec.rb
@@ -4,29 +4,32 @@ RSpec.feature 'admin can add/remove their active employees to a schedule' do
   scenario 'by clicking button on schedule page to add' do
     admin = build(:admin)
     sign_in admin
-    schedule    = create(:schedule, admin: admin)
-    shift_lead  = create(:employee, admin: admin)
-    team_member = create(:employee, first_name: 'Sam', position: 'Team Member')
+    schedule = create(:schedule, admin: admin)
+    create(:employee, admin: admin,
+                      first_name: 'Boaty',
+                      last_name:  'McBoatface')
+    create(:employee, first_name: 'Sam',
+                      last_name:  'McBoatface')
 
     visit schedule_path(schedule)
     click_on t('schedules.add_remove_employees.add_employees')
 
-    expect(page).to     have_content shift_lead.first_name_last_initial
-    expect(page).to_not have_content team_member.first_name_last_initial
+    expect(page).to     have_content('Boaty M.')
+    expect(page).to_not have_content('Sam M.')
   end
 
   scenario 'by clicking button on schedule page to remove' do
     admin = build(:admin)
     sign_in admin
-    schedule    = create(:schedule, admin: admin)
-    shift_lead  = create(:employee, admin: admin)
+    schedule = create(:schedule, admin: admin)
+    create(:employee, admin: admin,
+                      first_name: 'Boaty',
+                      last_name:  'McBoatface')
 
     visit schedule_path(schedule)
     click_on t('schedules.add_remove_employees.add_employees')
-
-    visit schedule_path(schedule)
     click_on t('schedules.add_remove_employees.remove_employees')
 
-    expect(page).to_not have_content shift_lead.first_name_last_initial
+    expect(page).to_not have_content('Boaty M.')
   end
 end
