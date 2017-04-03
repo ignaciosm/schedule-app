61af578ee5e3acb4e6549be5958bc454f8f896c3
thebrianemory <the@brianemory.com>
2017-04-03 07:42:19 -0700

    Remove unneeded db calls

--- spec/features/admin/admin_clicks_schedule_to_view_spec.rb
+++ spec/features/admin/admin_clicks_schedule_to_view_spec.rb
@@ -4,37 +4,39 @@ RSpec.feature 'Admin clicks schedule link' do
   scenario 'they see the schdule' do
     admin = create(:admin)
     sign_in admin
-    @schedule = create(:schedule, admin: admin)
+    @schedule = create(:schedule, admin: admin,
+                                  business_year: 2017,
+                                  business_week: 10)
 
     visit schedules_path
-    click_on @schedule.week_and_year
+    click_on 'Week 10, 2017'
 
     expect(page).to have_content t('schedules.show.greeting',
                                    date1: grab_date1,
                                    date7: grab_date7,
-                                   year: @schedule.business_year)
+                                   year: '2017')
   end
 
   scenario 'they see the week in the table header' do
     admin = create(:admin)
     sign_in admin
-    @schedule = create(:schedule, admin: admin)
+    @schedule = create(:schedule, admin: admin,
+                                  business_year: 2017,
+                                  business_week: 10)
 
     visit schedules_path
-    click_on @schedule.week_and_year
+    click_on 'Week 10, 2017'
 
     expect(page).to have_content t('schedules.table.header_days.header',
-                                   week: @schedule.business_week)
+                                   week: '10')
   end
 end
 
 # Make tests easier to read
 def grab_date1
-  Date.commercial(@schedule.business_year,
-                  @schedule.business_week, 1).strftime('%m/%d')
+  Date.commercial(2017, 10, 1).strftime('%m/%d')
 end
 
 def grab_date7
-  Date.commercial(@schedule.business_year,
-                  @schedule.business_week, 7).strftime('%m/%d')
+  Date.commercial(2017, 10, 7).strftime('%m/%d')
 end
