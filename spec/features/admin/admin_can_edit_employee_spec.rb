d08448ba6a811381c195d500533be4fda369a8ac
thebrianemory <the@brianemory.com>
2017-04-02 22:15:20 -0700

    Remove unneeded db calls

--- spec/features/admin/admin_can_edit_employee_spec.rb
+++ spec/features/admin/admin_can_edit_employee_spec.rb
@@ -4,17 +4,19 @@ RSpec.feature 'Admin can edit employee' do
   scenario 'they can see the employee\'s page' do
     admin = create(:admin)
     sign_in admin
-    create(:employee, admin: admin)
+    create(:employee, admin: admin,
+                      first_name: 'Boaty',
+                      last_name:  'McBoatface')
 
     visit root_path
     click_on t('layouts.navigation.admin_page')
     click_on t('admins.show.edit_employee')
     select('Team Member')
     click_on 'Update Employee'
-    employee = Employee.last
 
-    expect(employee.position).to eq 'Team Member'
-    expect(page).to have_content employee.first_name_last_initial
+    within('.team-members') do
+      expect(page).to have_content('Boaty M.')
+    end
   end
 
   scenario 're-renders edit page when submitting improper data' do
