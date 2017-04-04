3e87fea08f45cfd72f7ca6654a74e38123823595
thebrianemory <the@brianemory.com>
2017-04-03 22:22:58 -0700

    Update specs to be more explicit

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Shift, '.shifts_by_day(day)' do
   it 'returns shifts by day' do
-    shift1 = create(:shift)
+    shift1 = create(:shift, day_of_week: 'Monday')
     shift2 = create(:shift, day_of_week: 'Tuesday')
 
     expect(Shift.shifts_by_day('Monday')).to      eq [shift1]
@@ -13,7 +13,7 @@ RSpec.describe Shift, 'shifts_by_employee(employee)' do
     admin     = create(:admin)
     employee  = create(:employee)
     shift1    = create(:shift, employee_id: employee.id)
-    shift2    = create(:shift, employee_id: 0) # tests for admin shift
+    shift2    = create(:shift, employee_id: 0) # see note below
 
     expect(Shift.shifts_by_employee(employee)).to     eq [shift1]
     expect(Shift.shifts_by_employee(employee)).to_not eq [shift2]
@@ -24,10 +24,15 @@ end
 RSpec.describe Shift, '.shifts_by_day_and_employee(day, employee)' do
   it 'returns shifts by employee' do
     employee  = create(:employee)
-    shift1    = create(:shift, employee_id: employee.id)
+    shift1    = create(:shift, employee_id: employee.id, day_of_week: 'Monday')
     shift2    = create(:shift, employee_id: employee.id, day_of_week: 'Tuesday')
 
     expect(Shift.shifts_by_day_and_employee('Monday', employee)).to     eq [shift1]
     expect(Shift.shifts_by_day_and_employee('Monday', employee)).to_not eq [shift2]
   end
 end
+
+  # Note: employee_id is always 0 for admin shifts. This is to keep
+  # the id for the admin separate from the ids of the employees. Since there
+  # is only ever one admin per schedule, this works. It will need to be changed
+  # in the future if there are multiple admins.
