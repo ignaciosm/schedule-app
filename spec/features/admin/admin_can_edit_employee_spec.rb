0f57cfb363e3acb03019961552fce57d16e787a1
thebrianemory <the@brianemory.com>
2017-04-04 18:06:27 -0700

    Update spec to use I18n helper call

--- spec/features/admin/admin_can_edit_employee_spec.rb
+++ spec/features/admin/admin_can_edit_employee_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature 'Admin can edit employee' do
     click_on t('layouts.navigation.admin_page')
     click_on t('admins.show.edit_employee')
     select('Team Member')
-    click_on 'Update Employee'
+    click_on t('helpers.submit.update', model: Employee)
 
     within('.team-members') do
       expect(page).to have_content('Boaty M.')
@@ -28,7 +28,7 @@ RSpec.feature 'Admin can edit employee' do
     click_on  t('layouts.navigation.admin_page')
     click_on  t('admins.show.edit_employee')
     fill_in   'employee_first_name', with: ''
-    click_on  'Update Employee'
+    click_on  t('helpers.submit.update', model: Employee)
 
     expect(page).to have_content 'First name can\'t be blank'
   end
