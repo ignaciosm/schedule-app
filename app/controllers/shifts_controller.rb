8c47f855e516af7b010c6c95d3d038e8edf3827c
thebrianemory <the@brianemory.com>
2017-04-12 20:14:50 -0700

    Finish pulling shift creation from schedules ctrl Added a shifts controller to handle creation of shifts. Took the opportunity to change how the shift creation happened and to update the method to handle removing shifts. In the previous iteration empty shifts were ignored making it so you could not remove a shift from a schedule. This was not behavior I wanted. If both start_time and end_time are empty, it looks for a shift with that ID and deletes it. If one of the times are empty nothing happens. If both fields are filled in, the shift is created or updated depending on if it exists.

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,7 +2,11 @@ class ShiftsController < ApplicationController
   def set_shifts
     @schedule = Schedule.find(params[:id])
     shift_params[:shifts_attributes].values.each do |shift|
-      unless shift[:start_time].blank? || shift[:end_time].blank?
+      if shift[:start_time].blank? && shift[:end_time].blank?
+        @schedule.shifts.delete(shift[:id]) unless shift[:id].blank?
+      elsif shift[:start_time].blank? || shift[:end_time].blank?
+        next # do nothing if only one field is blank
+      else
         @updated_shift = @schedule.shifts.find_or_initialize_by(id: shift[:id])
         @updated_shift.update(start_time:  shift[:start_time],
                               end_time:    shift[:end_time],
