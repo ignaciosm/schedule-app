0e42f230f08e27ab9d22d7d9a1730ee4c83d5770
thebrianemory <the@brianemory.com>
2017-04-12 20:31:45 -0700

    Updated spec to look for empty fields

--- spec/features/admin/admin_can_set_a_schedule_spec.rb
+++ spec/features/admin/admin_can_set_a_schedule_spec.rb
@@ -18,10 +18,12 @@ RSpec.feature 'admin can set a schedule' do
     fill_in   'schedule_shifts_attributes_7_end_time',    with: '7:30 pm'
     fill_in   'schedule_shifts_attributes_14_start_time', with: '12:00 pm'
     fill_in   'schedule_shifts_attributes_14_end_time',   with: '4:00 pm'
+    fill_in   'schedule_shifts_attributes_15_end_time',   with: '7:00 pm'
     click_on  t('schedules.set_schedule.submit')
 
-    expect(page).to have_content '7:30 am - 4:00 pm'
-    expect(page).to have_content '3:30 pm - 7:30 pm'
-    expect(page).to have_content '12:00 pm - 4:00 pm'
+    expect(page).to     have_content '7:30 am - 4:00 pm'
+    expect(page).to     have_content '3:30 pm - 7:30 pm'
+    expect(page).to     have_content '12:00 pm - 4:00 pm'
+    expect(page).to_not have_content '7:00 pm'
   end
 end
